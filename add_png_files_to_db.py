64718ff515123c73db3a7a61500183980ab09dc5
Eric Abell <crucalculus@gmail.com>
2016-10-07 15:47:09 -0500

    add images to db
    PNGs are base64 encoded, added to the test
    database on local machine's MongoDB.

--- add_png_files_to_db.py
+++ add_png_files_to_db.py
@@ -1,3 +1,50 @@
 # assuming we have a bunch of png files
 # with chemical formulas as filenames,
 # we're going to add them to a MongoDB collection
+
+# get a list of all the png images in the current directory
+
+import os
+png_images=os.listdir('.')
+for fichier in png_images[:]: # png_images[:] makes a copy of png_images.
+    if not(fichier.endswith(".png")):
+        png_images.remove(fichier)
+
+import base64
+
+encoded_images_dict = {}
+
+for image in png_images:
+    encoded = base64.b64encode(open(image, "rb").read())
+    encoded_images_dict[os.path.splitext(image)[0]] = encoded
+
+print str(len(encoded_images_dict.keys())) + " encoded images are ready..."
+
+print encoded_images_dict.keys()
+
+
+# define the Mongo document structure
+
+from mongothon import Schema, create_model
+from pymongo import MongoClient
+
+connection = MongoClient('localhost', 27017)
+db = connection.test
+compounds = db['compounds']
+
+compound_schema = Schema({
+    "name": {"type": basestring, "required": True},
+    "formula_img": {"type": basestring, "required": True},
+    "notes": {"type": basestring},
+    "correct_count": {"type": basestring},
+    "incorrect_count": {"type": basestring}
+    })
+
+Compound = create_model( compound_schema, compounds )
+
+for new_compound in encoded_images_dict.keys():
+    compound = Compound({
+        "name": new_compound,
+        "formula_img": encoded_images_dict[new_compound]
+        })
+    compound.save()
